4b2903218cb916dfdbfc85cc693a1857f6eb54ef
Aamir Patel <paamir89@gmail.com>
2024-12-17 14:26:52 +0530

    Create vpcmain.tf

---
+++ vpcmain.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example_server" {
+  ami           = "ami-04e914639d0cca79a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "JacksBlogExample"
+  }
+}
